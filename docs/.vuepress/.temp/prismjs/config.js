6e583a182d8b6ca955835b6fe2932c3bc9eb125b
xujiahao <xujiahao@guanmai.cn>
2024-10-24 16:03:11 +0800

    feat: init-project

--- docs/.vuepress/.temp/prismjs/config.js
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -1,9 +1,9 @@
-import "D:/workspace/vuepress-starter/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
-import "D:/workspace/vuepress-starter/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
-import "D:/workspace/vuepress-starter/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
-import "D:/workspace/vuepress-starter/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
-import "D:/workspace/vuepress-starter/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
-import { setupCollapsedLines } from "D:/workspace/vuepress-starter/node_modules/@vuepress/highlighter-helper/lib/client/composables/collapsedLines.js"
+import "D:/workspace/gm-static-web/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "D:/workspace/gm-static-web/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "D:/workspace/gm-static-web/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "D:/workspace/gm-static-web/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "D:/workspace/gm-static-web/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "D:/workspace/gm-static-web/node_modules/@vuepress/highlighter-helper/lib/client/composables/collapsedLines.js"
 
 export default {
   setup() {
